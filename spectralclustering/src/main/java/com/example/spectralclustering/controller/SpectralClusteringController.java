f75bcc2c43e2d2da1b06354ef00024487a07ed0c
lab401\lwh <2391289924@qq.com>
2018-08-13 16:55:23 +0800

    Add cross origin declaration in the spectralclustering

--- spectralclustering/src/main/java/com/example/spectralclustering/controller/SpectralClusteringController.java
+++ spectralclustering/src/main/java/com/example/spectralclustering/controller/SpectralClusteringController.java
@@ -3,10 +3,7 @@ package com.example.spectralclustering.controller;
 import com.example.spectralclustering.dto.ClusterResult;
 import com.example.spectralclustering.service.SpectralClusteringSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cluster")
@@ -15,6 +12,7 @@ public class SpectralClusteringController {
     @Autowired
     private SpectralClusteringSerivce spectralClusteringSerivce;
 
+    @CrossOrigin(origins = "*")
     @GetMapping()
     public ClusterResult getClusterResult(@RequestParam(required = true) long endTs,
     		@RequestParam(required = true) long lookback, @RequestParam(required = true) int k){
