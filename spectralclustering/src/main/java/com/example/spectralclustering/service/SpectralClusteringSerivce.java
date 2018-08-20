e08ef980bdb47f7001b529407777103e1cb051fe
kylinxiang <15884286060@163.com>
2018-08-20 15:19:32 +0800

    remove spectralcluster module

--- spectralclustering/src/main/java/com/example/spectralclustering/service/SpectralClusteringSerivce.java
+++
@@ -1,7 +0,0 @@
-package com.example.spectralclustering.service;
-
-import com.example.spectralclustering.dto.ClusterResult;
-
-public interface SpectralClusteringSerivce {
-    ClusterResult getResult(long endTs, long lookback, int k);
-}
